ef5a1aba712ca127a86b97fbf87457920f3469b5
devcihat <cihattsungu@outlook.com>
2020-12-23 12:50:41 +0300

    1 file modified

--- src/components/Middle.jsx
+++ src/components/Middle.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Middle() {
   return (
     <div className="container text-center">
-      <div className="row row-cols-1 row-cols-md-3 g-4 pt-5">
+      <div className="row pt-4">
         <div className="col">
           <div className="card h-100">
             <img
